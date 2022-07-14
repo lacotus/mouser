a24301e5a7036d5978f8e3bfcb28d64dce456294
lacotus <lspiessebersole@gmail.com>
2022-07-14 17:53:18 -0400

    Further work on pynput module

--- src/mouser2.py
+++ src/mouser2.py
@@ -4,9 +4,34 @@ from pynput import keyboard
 import mouse
 import subprocess
 
+up = 'e'
+down = 'd'
+left = 's'
+right = 'f'
+
+speed_turbo = 'space'
+speed_slow = 'shift'
+
+click_left = 'j'
+click_middle = 'k'
+click_right = 'l'
+
+scroll_up = 'u'
+scroll_down = 'n'
+
+stop_key = ';'
+
+distance = 20
+duration = 0.1
+
+def move_mouse(x, y):
+    mouse.move(x, y, absolute=False, duration=duration)
+
 def on_press(key):
     try:
         print('alpha key {0} pressed'.format(key.char))
+        if (key.char == up):
+            move_mouse(0, -distance)
     except:
         print('special key {0} pressed'.format(key))
 
