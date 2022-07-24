cb0f1c1503d2ddda11c2f2894adc6ad224e257ce
lacotus <lspiessebersole@gmail.com>
2022-07-24 18:24:24 -0400

    Added Notes.txt, moved all 'Manage' functions into one function, added program map with labels

--- src/mouser.py
+++ src/mouser.py
@@ -5,11 +5,14 @@ import mouse
 import subprocess
 import threading
 
+# Comments with letters are based on 'Program map' in Notes.txt of the src directory
+
+# -=- A -=-
 no_key_pressed = True
 
 movement_keys = ['', ''] # spot 0 represents up/down while spot 1 represents left / right. captures movement.
-active_control = '' # for reading which control is active (probably used mostly for click_left and dragging)
-
+active_key = '' # for reading which control is active (probably used mostly for click_left and dragging)
+speed_key = '' # for reading if speed is set to slow or turbo
 # controls: 0 - up, 1 - down, 2 - left, 3 - right, 4 - speed_turbo, 5 - speed_slow, 6 - click_left, 7 - click_middle,
 #   8 - click_right, 9 - scroll_up, 10 - scroll_down, 11 - stop_key
 controls = ['e', 'd', 's', 'f', 'space', 'shift', 'j', 'k', 'l', 'u', 'n', ';']
@@ -35,6 +38,7 @@ stop_key = controls[11]
 distance = 20
 duration = 0.035
 
+# -=- B -=-
 def move_mouse():
     while True:
         if movement_keys == [up, '']:
@@ -76,45 +80,7 @@ def move_mouse():
 
         mouse.move((distance * x), (distance * y), absolute=False, duration=duration)
 
-
-def manage_movement_keys(called_from, key):
-    
-    # condition for catching if we're adding or removing
-    if called_from == 'set':
-        
-        # secion: adding
-        # condition that reads the key to find out what to add where
-        if key.char == up:
-            movement_keys[0] = up
-        
-        elif key.char == down:
-            movement_keys[0] = down
-
-        elif key.char == left:
-            movement_keys[1] = left
-
-        elif key.char == right:
-            movement_keys[1] = right
-        else:
-            print('Invalid character when adding to movement_keys')
-            exit()
-
-    elif called_from == 'remove':
-
-        # section: removing
-        # condition that reads the key to find out what to remove from where
-        if key.char == up or key.char == down:
-            movement_keys[0] = ''
-        elif key.char == left or key.char == right:
-            movement_keys[1] = ''
-        else:
-            print('Invalid character when removing from movement_keys')
-            exit()
-
-    else:
-        print('Please select either \'set\' or \'remove\' as the first arg when calling manage_movement_keys')
-        exit()
-
+# -=- C -=-
 def handle_click(key):
     if key.char == click_left:
         subprocess.call(["xdotool", "click", "1"])
@@ -134,78 +100,121 @@ def handle_click(key):
     elif key.char == stop_key:
         exit()
 
-def set_active_key(key):
+# -=- D -=-
+def manage_press(key):
     try:
-        active_key = key.char
-    except Exception as e:
-        print(e)
 
-def remove_active_key():
-    try:
-        active_key = ''
-    except Exception as e: 
-        print(e)
+        # Sort input into Movement, Click, Scroll/Stop, Speed
+        
+        # Movement
+        if key.char == up or key.char == down or key.char == left or key.char == right:
+            
+            # Movement keys, sets value based on what is read
+            if key.char == up:
+                movement_keys[0] = up
+        
+            elif key.char == down:
+                movement_keys[0] = down
 
-def manage_click(called_from, key):
-    try:
-        if called_from == 'set':
+            elif key.char == left:
+                movement_keys[1] = left
+
+            elif key.char == right:
+                movement_keys[1] = right
+            else:
+                print('Invalid character when adding to movement_keys')
+                exit()
+
+        # Click 
+        elif key.char == click_left or key.char == click_middle or key.char == click_right:
+            
+            # Sets active key
+            global active_key
             active_key = key.char
 
-        elif called_from == 'remove':
+            # Calls click
+            handle_click(key)
+
+        # Scroll/Stop
+        elif key.char == scroll_up or key.char == scroll_down or key.char == stop_key:
+            
+            # Call click, no need to record if these keys are being held yet
+            handle_click(key)
+
+        elif key == speed_turbo or key == speed_slow:
+            # TODO - handle speed keys
+            global speed_key
+            speed_key = speed_turbo
+            
+
+    except Exception as e:
+        print(e)    
+
+# -=- E -=-
+def manage_release(key):
+    try:
+        
+        # Sort input into Movement, Click, Speed
+
+        # Movement
+        if key.char == up or key.char == down or key.char == left or key.char == right:
+            
+            # Movement keys, sets value based on what is read
+            if key.char == up:
+                movement_keys[0] = ''
+        
+            elif key.char == down:
+                movement_keys[0] = ''
+
+            elif key.char == left:
+                movement_keys[1] = ''
+
+            elif key.char == right:
+                movement_keys[1] = ''
+            else:
+                print('Invalid character when adding to movement_keys')
+                exit()
+
+        # Click
+        elif key.char == click_left or key.char == click_middle or key.char == click_right:
+            
+            # Sets active key
+            global active_key
             active_key = ''
 
-        else:
-            print('Please select either \'set\' or \'remove\' as the first arg when calling manage_movement_keys')
-            exit()
+ 
 
     except Exception as e:
         print(e)
 
+# -=- F -=-
 def on_press(key):
     try:
-        global no_key_pressed
-
-        # To console
-        print('alpha key {0} pressed'.format(key.char))
-
-        # Set movement_keys
-        if key.char == up or key.char == down or key.char == left or key.char == right:
-            manage_movement_keys('set', key)
-            
-            # Move the mouse
-            if movement_keys != ['', ''] and no_key_pressed == True:
-                no_key_pressed = False
-                thread = threading.Thread(target=move_mouse)
-                thread.start()
-  
-        else:
-            # Handle all clicking / non-moving keys
-            manage_click('set', key)
-            handle_click(key)
+        manage_press(key)
         
-        
-        
-        print("Active keys: " + ' '.join(movement_keys))
+        print("Movement keys: " + ' '.join(movement_keys))
+        print("Speed key: " + speed_key)
+        print("Active key: " + active_key)
     except Exception as e:
         print('special key {0} pressed'.format(key))
         print(e)
 
+# -=- G -=-
 def on_release(key):
     print('{0} released'.format(key))
     try:
-        # Removes the keys from movement_keys as they are being released
-        if key.char == up or key.char == down or key.char == left or key.char == right:
-            manage_movement_keys('remove', key)
-
-        else:
-            manage_click(key)
+        manage_release(key)
         
         print("Active keys: " + ' '.join(movement_keys))
     except Exception as e:
         print(e)
 
+# -=- H -=-
+# Starts threading move_mouse()
+thread = threading.Thread(target=move_mouse)
+thread.start()
 
-
+# -=- I -=-
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release,
